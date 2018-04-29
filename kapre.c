9b11fe9766c9551d74c9c02a40c2767185260349
ferranschen <ferrans.chen@gmail.com>
2018-04-29 21:40:04 +0800

    add Hash functions

--- kapre.c
+++ kapre.c
@@ -24,7 +24,8 @@ typedef struct FIRST_LEVEL_PARAMETER{
 } first_level_parameter;
 
 
-
+unsigned long H(element_t first, element_t second, pairing_t pairing);
+unsigned long H1(element_t first, element_t second, pairing_t pairing);
 unsigned long hash(unsigned char *str);
 
 
@@ -43,6 +44,7 @@ int main(int argc, char **argv) {
   element_t tempG1;
   element_t tempGT;
   element_t number[100]; 
+  element_t Z;
   secret_key AliceSecretKey;
   secret_key BobSecretKey;
   public_key AlicePublicKey;
@@ -62,7 +64,7 @@ int main(int argc, char **argv) {
   element_init_G1(tempG1, pairing);
   element_init_GT(tempGT, pairing);
   element_init_Zr(tempZR, pairing);
-
+  element_init_GT(Z, pairing);
 
   for(int i = 0; i < 100; i++) element_init_Zr(number[i], pairing);
   for(int i = 0; i < 100; i++) element_set_si(number[i], i); // set nature number 0 ~ 100
@@ -116,6 +118,8 @@ int main(int argc, char **argv) {
   element_random(BobSecretKey.a1);
   element_random(BobSecretKey.a2);
   element_random(BobSecretKey.a3);
+  element_pairing(Z, g, g);
+  element_printf("system parameter Z = %B\n", Z);
   // Key Generation
   element_pow_zn(AlicePublicKey.part1, g, AliceSecretKey.a1); 
   element_pow_zn(AlicePublicKey.part2, g, AliceSecretKey.a2);
@@ -166,12 +170,16 @@ int main(int argc, char **argv) {
   element_mul(r2, AlicePublicKey.part3[2], AlicePublicKey.part3[1]); // We assume ALice share file set S = {1,2}
   element_pow_zn(r2, r2, AliceSecretKey.a2); //  
   // Enc2
+  element_t K;
+  element_init_GT(K, pairing);
+  unsigned long m = 1; //you can specify any number, here we use 1 as an example 
   unsigned char *data;
   int length = 0;
   element_random(AliceSecondLevelParameter.t);
   element_random(AliceSecondLevelParameter.k);
   element_random(AliceSecondLevelParameter.r);
   element_random(AliceSecondLevelParameter.n);
+  element_pow_zn(K, Z, AliceSecondLevelParameter.r);
   element_set(AliceSecondLevelCiphertext.c0, AliceSecondLevelParameter.k);
   element_pow_zn(AliceSecondLevelCiphertext.c1, d, AliceSecondLevelParameter.r);
   element_pow_zn(AliceSecondLevelCiphertext.c2, g, AliceSecretKey.a1);
@@ -180,7 +188,13 @@ int main(int argc, char **argv) {
   element_pow_zn(AliceSecondLevelCiphertext.c4, g, AliceSecretKey.a2);
   element_mul(AliceSecondLevelCiphertext.c4, AliceSecondLevelCiphertext.c4, AlicePublicKey.part3[0]);
   element_pow_zn(AliceSecondLevelCiphertext.c4, AliceSecondLevelCiphertext.c4, AliceSecondLevelParameter.t);
-  length = pairing_length_in_bytes_G1(pairing);
+  element_pow_zn(tempG1, d, AliceSecondLevelParameter.r); // tempG1 = d^r
+  printf("H1(...) = %lu\n ", H1(K, tempG1, pairing));
+  m = m ^ H1(K, tempG1, pairing);
+  printf("H1(...) = %lu\n ", m);
+/*  length = pairing_length_in_bytes_G1(pairing);
+  unsigned long m;
+  insigned long m;
   data = malloc(length);
   element_to_bytes(data, AliceSecondLevelCiphertext.c2);
   data[length] = '\0'; 
@@ -192,14 +206,14 @@ int main(int argc, char **argv) {
   printf("hash value:%lu\n",hash(data));
   printf("hash value:%lu\n",hash(data));
   printf("hash value:%lu\n",hash(data));
-
+*/
   // Enc1
   element_random(AliceFirstLevelParameter.k);
   element_random(AliceFirstLevelParameter.r);
   element_random(AliceFirstLevelParameter.n);
   element_set(AliceFirstLevelCiphertext.c0, AliceFirstLevelParameter.k);
   // Re-encrption
-  
+    
   // Dec1
 
   // Dec2
@@ -208,6 +222,75 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+unsigned long H1(element_t first, element_t second, pairing_t pairing)
+{
+  unsigned char *firstString = NULL;
+  unsigned char *secondString = NULL;
+  unsigned long firstHashValue = 0;
+  unsigned long secondHashValue = 0;
+  unsigned long ret = 0;
+  int GTlength = 0;
+  int G1length = 0;
+  GTlength = pairing_length_in_bytes_G1(pairing);
+  G1length = pairing_length_in_bytes_G1(pairing);
+  firstString = malloc(GTlength);
+  secondString = malloc(G1length);
+  if(firstString == NULL || secondString == NULL)
+  {
+    pbc_error("Memory allocation failed.");
+    return 0;
+  }
+  if(element_to_bytes(firstString, first) <= 0 || element_to_bytes(secondString, second) <= 0)
+  {
+    pbc_error("element_to_bytes failed.");
+    return 0;
+  }
+  firstHashValue = hash(firstString);
+  secondHashValue = hash(secondString);
+  if(firstHashValue <= 0 || secondHashValue <= 0)
+  {
+    pbc_error("hash failed.");
+    return 0;
+  }
+  ret = firstHashValue + secondHashValue;
+  free(firstString);
+  free(secondString);
+  return ret;
+}
+
+unsigned long H(element_t first, element_t second, pairing_t pairing)
+{
+  unsigned char *firstString = NULL;
+  unsigned char *secondString = NULL;
+  unsigned long firstHashValue = 0;
+  unsigned long secondHashValue = 0;
+  unsigned long ret = 0;
+  int length = 0;
+  length = pairing_length_in_bytes_G1(pairing);
+  firstString = malloc(length);
+  secondString = malloc(length);
+  if(firstString == NULL || secondString == NULL)
+  {
+    pbc_error("Memory allocation failed.");
+    return 0;
+  }
+  if(element_to_bytes(firstString, first) <= 0 || element_to_bytes(secondString, second) <= 0)
+  {
+    pbc_error("element_to_bytes failed.");
+    return 0;
+  }
+  firstHashValue = hash(firstString);
+  secondHashValue = hash(secondString);
+  if(firstHashValue <= 0 || secondHashValue <= 0)
+  {
+    pbc_error("hash failed.");
+    return 0;
+  }
+  ret = firstHashValue + secondHashValue;
+  free(firstString);
+  free(secondString);
+  return ret;
+}
 
 unsigned long hash(unsigned char *str)
 {
